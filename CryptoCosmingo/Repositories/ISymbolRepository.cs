f1a5695430bd81878d9920f7b9e1ee880a1d11b8
CosminTG <sergio.sr11090@gmail.com>
2026-02-14 13:54:31 -0500

    feat: add ISymbolRepository interface
    Defines GetAllAsync and CreateAsync methods for symbol data access

--- CryptoCosmingo/Repositories/ISymbolRepository.cs
+++ CryptoCosmingo/Repositories/ISymbolRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace CryptoCosmingo.Repositories
+﻿using CryptoCosmingo.Models;
+
+namespace CryptoCosmingo.Repositories
 {
     public interface ISymbolRepository
     {
+        Task<List<Symbol>> GetAllAsync();
+        Task CreateAsync(Symbol symbol);
+
     }
 }
