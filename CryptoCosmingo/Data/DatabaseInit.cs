bb10d4741ab1e08090ca1f103df7f01bc036709c
CosminTG <sergio.sr11090@gmail.com>
2026-02-14 13:34:08 -0500

    feat CryptoCosmingo/Data/DatabaseInit.cs added CreateTables method

--- CryptoCosmingo/Data/DatabaseInit.cs
+++ CryptoCosmingo/Data/DatabaseInit.cs
@@ -1,6 +1,21 @@
-﻿namespace CryptoCosmingo.Data
+﻿using Microsoft.Data.Sqlite;
+
+namespace CryptoCosmingo.Data
 {
     public class DatabaseInit
     {
+        public static void CreateTables(string connectionString)
+        {
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            using var command = connection.CreateCommand();
+            command.CommandText = 
+                @"CREATE TABLE IF NOT EXISTS Symbols(
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Name TEXT NOT NULL);
+                ";
+            command.ExecuteNonQuery();
+        }
     }
 }
