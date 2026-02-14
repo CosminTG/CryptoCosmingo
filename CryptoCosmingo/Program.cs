27d17bd6229a1ee52c8fd2618451bbdb8daf3aa9
CosminTG <sergio.sr11090@gmail.com>
2026-02-14 15:43:30 -0500

    config: setup dependency injection and database initialization

--- CryptoCosmingo/Program.cs
+++ CryptoCosmingo/Program.cs
@@ -1,7 +1,34 @@
+using CryptoCosmingo.Config;
+using CryptoCosmingo.Data;
+using CryptoCosmingo.Repositories;
+using CryptoCosmingo.Services;
 using DotNetEnv;
+
 var builder = WebApplication.CreateBuilder(args);
+
 Env.Load();
+
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
+
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new Exception("DB_CONNECTION no está configurada en el archivo .env");
+}
+
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSingleton(new DatabaseConfig
+{
+    ConnectionString = connectionString
+});
+
+builder.Services.AddScoped<ISymbolRepository, SymbolRepository>();
+builder.Services.AddScoped<ISymbolService, SymbolService>();
+
 var app = builder.Build();
+
+DatabaseInit.CreateTables(connectionString);
+
 app.MapControllers();
 app.Run();
