37510924b818a2879ef0c978bf8497a3b53612c1
CosminTG <sergio.sr11090@gmail.com>
2026-02-14 15:23:33 -0500

    feat: implement SymbolService with DTO mapping
    - Add GetAllAsync with Symbol to SymbolDTO conversion
    - Add CreateAsync with CreateSymbolDTO to Symbol mapping

--- CryptoCosmingo/Services/SymbolService.cs
+++ CryptoCosmingo/Services/SymbolService.cs
@@ -1,6 +1,37 @@
-﻿namespace CryptoCosmingo.Services
+﻿using CryptoCosmingo.DTOs;
+using CryptoCosmingo.Models;
+using CryptoCosmingo.Repositories;
+
+namespace CryptoCosmingo.Services
 {
-    public class SymbolService
+    public class SymbolService : ISymbolService
     {
+        private readonly ISymbolRepository _repo;
+
+        public SymbolService(ISymbolRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<List<SymbolDTO>> GetAllAsync()
+        {
+            var symbols = await _repo.GetAllAsync();
+
+            return symbols.Select(x => new SymbolDTO
+            {
+                Id = x.Id,
+                Name = x.Name
+            }).ToList();
+        }
+
+        public async Task CreateAsync(CreateSymbolDTO dto)
+        {
+            var symbol = new Symbol
+            {
+                Name = dto.Name
+            };
+
+            await _repo.CreateAsync(symbol);
+        }
     }
 }
