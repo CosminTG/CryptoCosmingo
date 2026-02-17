47d2d19c83dc035eb3b4bd2f1da8283a4377dc30
CosminTG <sergio.sr11090@gmail.com>
2026-02-17 11:38:17 -0500

    refactor: reorganize Program.cs structure

--- CryptoCosmingo/Program.cs
+++ CryptoCosmingo/Program.cs
@@ -7,28 +7,23 @@ using DotNetEnv;
 var builder = WebApplication.CreateBuilder(args);
 
 Env.Load();
-
 var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
-
 if (string.IsNullOrEmpty(connectionString))
 {
     throw new Exception("DB_CONNECTION no está configurada en el archivo .env");
 }
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSingleton(new DatabaseConfig
 {
     ConnectionString = connectionString
 });
-
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddScoped<ISymbolRepository, SymbolRepository>();
 builder.Services.AddScoped<ISymbolService, SymbolService>();
 
-var app = builder.Build();
-
 DatabaseInit.CreateTables(connectionString);
 
+var app = builder.Build();
 app.MapControllers();
 app.Run();
